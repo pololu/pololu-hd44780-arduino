fdd0162f05994b13b22ad33a70f28eadde2a46c5
David (Pololu) <dev-david@pololu.com>
2014-12-02 12:13:03 -0800

    Added keywords.txt.

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,9 +4,6 @@
   (c-mode . ((c-basic-offset . 4) (tab-width . 4) (indent-tabs-mode . nil)))
   (nil . ((fill-column . 80)))
   ("PololuHD44780/examples" . (
-    (nil . ((fill-column . 65)))
-  ))
-  ("PololuHD44780/keywords.txt" . (
-    (nil . ((indent-tabs-mode . t)))
+    (nil . ((c-basic-offset . 2) (tab-width . 2) (fill-column . 65)))
   ))
 )
