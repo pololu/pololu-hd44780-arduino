754e38b62feef200c37903389a92b4c603323c4b
David (Pololu) <dev-david@pololu.com>
2014-12-01 18:33:17 -0800

    Test.ino: Made it use PololuHD44780 instead of a custom class.  Made it display well in the default Arduino IDE width of 65 characters.

--- PololuHD44780/examples/Test/Test.ino
+++ PololuHD44780/examples/Test/Test.ino
@@ -1,64 +1,12 @@
-#include <Wire.h>
-#include <FastGPIO.h>
-#include <USBPause.h>  // TODO: change to UsbPause.h after that library is released
+/** Test program for the PololuHD44780 library which test all of
+ * the functions it defines.  This program is mainly meant as a
+ * test suite for library developers who want to make sure that
+ * their changes did not break any previously-working features of
+ * the library. */
+
 #include <PololuHD44780.h>
-#include <util/delay.h>
 
-// TODO: change this to use PololuHD44780, remove this class and make sure PololuHD44780 works
-
-class MyLCD : public PololuHD44780Base
-{
-    static const uint8_t rs = 7;
-    static const uint8_t e = 8;
-    static const uint8_t db4 = 14;
-    static const uint8_t db5 = 17;
-    static const uint8_t db6 = 13;
-    static const uint8_t db7 = IO_D5;
-
-public:
-
-    virtual void initPins()
-    {
-        FastGPIO::Pin<e>::setOutputLow();
-    }
-
-    virtual void send(uint8_t data, bool rsValue, bool only4bits)
-    {
-        FastGPIO::Pin<rs>::setOutput(rsValue);
-
-        USBPause usbPause;
-        FastGPIO::PinLoan<db4> loanDB4;
-        FastGPIO::PinLoan<db5> loanDB5;
-        FastGPIO::PinLoan<db6> loanDB6;
-        FastGPIO::PinLoan<db7> loanDB7;
-
-        if (!only4bits)
-        {
-            sendNibble(data >> 4);
-        }
-        sendNibble(data & 0x0F);
-    }
-
-private:
-    void sendNibble(uint8_t data)
-    {
-        FastGPIO::Pin<db4>::setOutput(data >> 0 & 1);
-        FastGPIO::Pin<db5>::setOutput(data >> 1 & 1);
-        FastGPIO::Pin<db6>::setOutput(data >> 2 & 1);
-        FastGPIO::Pin<db7>::setOutput(data >> 3 & 1);
-
-        // Send a pulse on the Enable line.  According to the "Bus
-        // Timing Characteristics" section of the HD44780 datasheet,
-        // 450 ns is the minimum enable pulse width (PW_EH) and the
-        // minimum enable cycle time is 1000 ns.
-        FastGPIO::Pin<e>::setOutputHigh();
-        _delay_us(1);   // At least 450 ns.
-        FastGPIO::Pin<e>::setOutputLow();
-        _delay_us(1);   // At least 550 ns.
-    }
-};
-
-MyLCD lcd;
+PololuHD44780 lcd(7, 6, 5, 4, 3, 2);
 
 void loadCustomCharacters()
 {
@@ -75,8 +23,9 @@ void loadCustomCharacters()
     };
     lcd.loadCustomCharacter(smiley, 0);
 
-    // Make sure that old code that uses 'char' for the pictures still works
-    // because the OrangutanLCD function took char* instead of uint8_t*.
+    // Make sure that old code that uses 'char' for the pictures
+    // still works because the OrangutanLCD function took char*
+    // instead of uint8_t*.
     static const char back_arrow[] PROGMEM = {
 	0b00000,
 	0b00010,
@@ -104,11 +53,11 @@ void loadCustomCharacters()
 
 void setup()
 {
-  loadCustomCharacters();
 }
 
-// Prints the ID of the screen we are currently showing and waits for
-// the user to send input on the serial monitor before continuing.
+// Prints the ID of the screen we are currently showing and waits
+// for the user to send input on the serial monitor before
+// continuing.
 void wait(uint16_t id)
 {
   SERIAL_PORT_MONITOR.println(id);
@@ -119,6 +68,7 @@ void wait(uint16_t id)
 void loop()
 {
   lcd.reinitialize();
+  loadCustomCharacters();
 
   // Test both overloads of write
   // Expected screen: "Hello   "
@@ -137,7 +87,7 @@ void loop()
   lcd.write("1:\x01");
   wait(10);
 
-  // Test print and goto
+  // Test print and gotoXY
   // Expected screen: "Hi there"
   //                  "12345678"
   lcd.clear();
@@ -186,36 +136,42 @@ void loop()
   wait(90);
 
   // Test scrollDisplayLeft.
-  // Expected screen: "i there " (with a blinking cursor on the second column)
+  // Expected screen: "i there "
   //                  "23456789"
+  // (with a blinking cursor on the second column, top row)
   lcd.scrollDisplayLeft();
   wait(100);
 
   // Test scrollDisplayLeft.
-  // Expected screen: " there  " (with a blinking cursor on the first column)
+  // Expected screen: " there  "
   //                  "3456789a"
+  // (with a blinking cursor on the first column, top row)
   lcd.scrollDisplayLeft();
   wait(110);
 
   // Test scrollDisplayRight.
-  // Expected screen: "i there " (with a blinking cursor on the second column)
+  // Expected screen: "i there "
   //                  "23456789"
+  // (with a blinking cursor on the second column, top row)
   lcd.scrollDisplayRight();
   wait(120);
 
   // Test home.
-  // Expected screen: "Hi there" (with a blinking cursor over H)
+  // Expected screen: "Hi there"
   //                  "12345678"
+  // (with a blinking cursor on the H)
   lcd.home();
   wait(130);
 
-  // Test clear().  The screen should be empty except for a blinking cursor in the upper left.
+  // Test clear().  The screen should be empty except for a
+  // blinking cursor in the upper left.
   lcd.clear();
   wait(140);
 
   // Test leftToRight and rightToLeft
   // Expected screen: "abcdef  "
   //                  "        "
+  // (with a blinking cursor)
   lcd.print("abXX");
   lcd.rightToLeft();
   lcd.print("Xdc");
@@ -233,14 +189,16 @@ void loop()
   lcd.print("klmn");
   wait(160);
 
-  // Test that the screen can really hold 40 characters on each line,
-  // and that it can display the last 4 columns with the first 4 columns.
+  // Test that the screen can really hold 40 characters on each
+  // line, and that it can display the last 4 columns with the
+  // first 4 columns.
   // Expected screen: "!@#$abcd"
   //                  "%^&*ABCD"
+  // (with a blinking cursor over the a)
   lcd.clear();
-  lcd.print("abcdefghijklmnopqrstuvwxyz0123456789!@#$--");
+  lcd.print("abcdefghijklmnopqrstuvwxyz0123456789!@#$");
   lcd.gotoLine(1);
-  lcd.print("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789%^&*--");
+  lcd.print("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789%^&*");
   lcd.scrollDisplayRight();
   lcd.scrollDisplayRight();
   lcd.scrollDisplayRight();
