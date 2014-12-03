7ec55958e3e60424ee8b355bcf4cdd2f2db7b723
David (Pololu) <dev-david@pololu.com>
2014-12-03 10:43:37 -0800

    Emacs settings: Display tabs as 8 characters in the examples because that is what the Arduino IDE does.  But we should avoid tabs anyway.

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,6 +4,6 @@
   (c-mode . ((c-basic-offset . 4) (tab-width . 4) (indent-tabs-mode . nil)))
   (nil . ((fill-column . 80)))
   ("PololuHD44780/examples" . (
-    (nil . ((c-basic-offset . 2) (tab-width . 2) (fill-column . 65)))
+    (nil . ((c-basic-offset . 2) (tab-width . 8) (fill-column . 65)))
   ))
 )
